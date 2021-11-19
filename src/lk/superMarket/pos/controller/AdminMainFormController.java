53de97cf3ea9a9d74ffe80eaa9b8466d2b993ab2
Kavishka <kavishkaprabath21@gmail.com>
2021-11-19 07:29:26 +0530

    Add Admin and Cashier main form UI and Controller

---
+++ src/lk/superMarket/pos/controller/AdminMainFormController.java
@@ -0,0 +1,106 @@
+package lk.superMarket.pos.controller;
+
+import javafx.animation.FadeTransition;
+import javafx.animation.ScaleTransition;
+import javafx.animation.TranslateTransition;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.effect.DropShadow;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.paint.Color;
+import javafx.stage.Stage;
+import javafx.util.Duration;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+/**
+ * @author : Kavishka Prabath
+ * @since : 0.1.0
+ **/
+
+public class AdminMainFormController {
+    public AnchorPane root;
+    public ImageView imgItem;
+    public ImageView imgBusinessIncome;
+    public ImageView imgItemMove;
+    public ImageView imgLogOut;
+    public ImageView imgViewReport;
+    public ImageView imgCustomerIncome;
+    public Label lblDescription;
+    public Label lblMenu;
+
+    public void initialize(URL url, ResourceBundle rb) {
+        FadeTransition fadeIn = new FadeTransition(Duration.millis(2000), root);
+        fadeIn.setFromValue(0.0);
+        fadeIn.setToValue(1.0);
+        fadeIn.play();
+    }
+
+    public void navigate(MouseEvent event) throws IOException {
+    }
+
+    public void playMouseEnterAnimation(MouseEvent event) {
+        if (event.getSource() instanceof ImageView) {
+            ImageView icon = (ImageView) event.getSource();
+
+            switch (icon.getId()) {
+                case "imgCustomerIncome":
+                    lblMenu.setText("Customer Wise Income");
+                    lblDescription.setText("Click to View Customer wise Income");
+                    break;
+                case "imgItem":
+                    lblMenu.setText("Manage Items");
+                    lblDescription.setText("Click to add, edit, delete, search or view items");
+                    break;
+                case "imgItemMove":
+                    lblMenu.setText("Item Movable");
+                    lblDescription.setText("Click to view most movable items and least movable items");
+                    break;
+                case "imgBusinessIncome":
+                    lblMenu.setText("Business Income");
+                    lblDescription.setText("Click to view daily, monthly or annual income");
+                    break;
+                case "imgViewReport":
+                    lblMenu.setText("Reports");
+                    lblDescription.setText("Click to view all reports");
+                    break;
+                case "imgLogOut":
+                    lblMenu.setText("LogOut");
+                    lblDescription.setText("Click here if you want to LogOut in Cashier Form");
+                    break;
+            }
+
+            ScaleTransition scaleT = new ScaleTransition(Duration.millis(200), icon);
+            scaleT.setToX(1.2);
+            scaleT.setToY(1.2);
+            scaleT.play();
+
+            DropShadow glow = new DropShadow();
+            glow.setColor(Color.CORNFLOWERBLUE);
+            glow.setWidth(20);
+            glow.setHeight(20);
+            glow.setRadius(20);
+            icon.setEffect(glow);
+        }
+    }
+
+    public void playMouseExitAnimation(MouseEvent event) {
+        if (event.getSource() instanceof ImageView) {
+            ImageView icon = (ImageView) event.getSource();
+            ScaleTransition scaleT = new ScaleTransition(Duration.millis(200), icon);
+            scaleT.setToX(1);
+            scaleT.setToY(1);
+            scaleT.play();
+
+            icon.setEffect(null);
+            lblMenu.setText("Welcome");
+            lblDescription.setText("Please select one of above main operations to proceed");
+        }
+    }
+}
---
+++ src/lk/superMarket/pos/controller/CashierMainFormController.java
@@ -0,0 +1,92 @@
+package lk.superMarket.pos.controller;
+
+import javafx.animation.FadeTransition;
+import javafx.animation.ScaleTransition;
+import javafx.animation.TranslateTransition;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.effect.DropShadow;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.paint.Color;
+import javafx.stage.Stage;
+import javafx.util.Duration;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+/**
+ * @author : Kavishka Prabath
+ * @since : 0.1.0
+ **/
+
+public class CashierMainFormController {
+    public AnchorPane root;
+    public ImageView imgOrders;
+    public ImageView imgCustomer;
+    public Label lblDescription;
+    public Label lblMenu;
+    public ImageView imgLogOut;
+
+    public void initialize(URL url, ResourceBundle rb) {
+        FadeTransition fadeIn = new FadeTransition(Duration.millis(2000), root);
+        fadeIn.setFromValue(0.0);
+        fadeIn.setToValue(1.0);
+        fadeIn.play();
+    }
+
+    public void navigate(MouseEvent event) throws IOException {
+
+    }
+
+    public void playMouseEnterAnimation(MouseEvent event) {
+        if (event.getSource() instanceof ImageView) {
+            ImageView icon = (ImageView) event.getSource();
+
+            switch (icon.getId()) {
+                case "imgCustomer":
+                    lblMenu.setText("Manage Customers");
+                    lblDescription.setText("Click to add, edit, delete, search or view customers");
+                    break;
+                case "imgOrders":
+                    lblMenu.setText("Place Orders");
+                    lblDescription.setText("Click here if you want to place a new order");
+                    break;
+                case "imgLogOut":
+                    lblMenu.setText("LogOut");
+                    lblDescription.setText("Click here if you want to LogOut in Cashier Form");
+                    break;
+            }
+
+            ScaleTransition scaleT = new ScaleTransition(Duration.millis(200), icon);
+            scaleT.setToX(1.2);
+            scaleT.setToY(1.2);
+            scaleT.play();
+
+            DropShadow glow = new DropShadow();
+            glow.setColor(Color.CORNFLOWERBLUE);
+            glow.setWidth(20);
+            glow.setHeight(20);
+            glow.setRadius(20);
+            icon.setEffect(glow);
+        }
+    }
+
+    public void playMouseExitAnimation(MouseEvent event) {
+        if (event.getSource() instanceof ImageView) {
+            ImageView icon = (ImageView) event.getSource();
+            ScaleTransition scaleT = new ScaleTransition(Duration.millis(200), icon);
+            scaleT.setToX(1);
+            scaleT.setToY(1);
+            scaleT.play();
+
+            icon.setEffect(null);
+            lblMenu.setText("Welcome");
+            lblDescription.setText("Please select one of above main operations to proceed");
+        }
+    }
+}
