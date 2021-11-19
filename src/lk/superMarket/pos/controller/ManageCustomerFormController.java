8a2587221aa2db24d7bef250f117ff854366f6e3
Kavishka <kavishkaprabath21@gmail.com>
2021-11-20 01:58:47 +0530

    Add Customer form complete ui

---
+++ src/lk/superMarket/pos/controller/ManageCustomerFormController.java
@@ -0,0 +1,42 @@
+package lk.superMarket.pos.controller;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.*;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
+
+/**
+ * @author : Kavishka Prabath
+ * @since : 0.1.0
+ **/
+
+public class ManageCustomerFormController {
+    public AnchorPane root;
+    public TextField txtCustomerId;
+    public ComboBox cmbCustomerTitle;
+    public TextField txtCustomerName;
+    public TextArea txtCustomerAddress;
+    public TextField txtCustomerCity;
+    public ComboBox cmbCustomerProvince;
+    public TextField txtCustomerPostalCode;
+    public TableView tblCustomers;
+    public Button btnSearch;
+    public Button btnAddNewCustomer;
+    public Button btnSave;
+    public Button btnDelete;
+
+    public void btnSearch_OnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnAddNew_OnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnSave_OnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnDelete_OnAction(ActionEvent actionEvent) {
+    }
+
+    public void navigateToHome(MouseEvent mouseEvent) {
+    }
+}
