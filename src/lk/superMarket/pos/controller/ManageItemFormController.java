ac8c691516157fe71fe81b777029fc5a6fdbfb59
Kavishka <kavishkaprabath21@gmail.com>
2021-11-21 16:12:58 +0530

    Add Item Form complete ui

---
+++ src/lk/superMarket/pos/controller/ManageItemFormController.java
@@ -0,0 +1,41 @@
+package lk.superMarket.pos.controller;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.*;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
+
+/**
+ * @author : Kavishka Prabath
+ * @since : 0.1.0
+ **/
+
+public class ManageItemFormController {
+    public AnchorPane root;
+    public TextField txtItemCode;
+    public TextArea txtItemDescription;
+    public ComboBox cmbItemSize;
+    public TextField txtItemUnitPrice;
+    public TextField txtItemQtyOnHand;
+    public TextField txtDiscount;
+    public TableView tblItems;
+    public Button btnAddNewItem;
+    public Button btnSave;
+    public Button btnDelete;
+    public Button btnSearch;
+
+    public void navigateToBack(MouseEvent mouseEvent) {
+    }
+
+    public void btnAddNew_OnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnSave_OnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnDelete_OnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnSearch_OnAction(ActionEvent actionEvent) {
+    }
+}
